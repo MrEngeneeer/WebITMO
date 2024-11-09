43e7d8197529fc9f0b7305d5e7cc26ef833c0e7c
Ganetski Ivan <palayf@mail.ru>
2024-11-09 13:31:30 +0300

    lab4

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('nav ul');
 
-    menuToggle.addEventListener('click', function() {
+    menuToggle.addEventListener('click', function () {
         navMenu.classList.toggle('show');
     });
 });
+
+(function () {
+
+    window.addEventListener("load", function () {
+        const loadTime = performance.now();
+        const footer = document.querySelector("footer");
+        const loadInfo = document.createElement("p");
+        loadInfo.textContent = `Время загрузки страницы: ${loadTime.toFixed(2)} мс`;
+        footer.appendChild(loadInfo);
+        const menuItems = document.querySelectorAll('nav a');
+        const currentPath = document.location.pathname.split("/").pop();
+
+        menuItems.forEach((item) => {
+            if (item.getAttribute("href").split("/").pop() === currentPath) {
+                item.classList.add("active");
+            }
+        });
+    });
+
+})();
